957334896447a8fa9b822246d1382d8a9696ae94
luanferro <luanferror@gmail.com>
2026-04-25 13:04:44 -0300

    first commit

---
+++ src/test/java/com/luanferro/reservation_api/ReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanferro.reservation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
