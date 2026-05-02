bfffaa88fe6daa9bd6245c240671a0bb409b03bb
luanferro <luanferror@gmail.com>
2026-05-01 22:58:24 -0300

    feat: implements entrypoint and custom handler for spring security

--- src/main/java/com/luanferro/reservation_api/config/security/SecurityConfig.java
+++ src/main/java/com/luanferro/reservation_api/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.luanferro.reservation_api.config.security;
 
+import com.luanferro.reservation_api.adapters.in.security.AuthEntryPoint;
+import com.luanferro.reservation_api.adapters.in.security.CustomAccessDeniedHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final SecurityFilter securityFilter;
+    private final AuthEntryPoint authEntryPoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,6 +39,10 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,
                                 "/table").hasRole("ADMIN")
                         .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(authEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler)
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
